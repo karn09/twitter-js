80e5d696975c9faacd52849ff2c2a57447740aa6
John Nieves <karn09@gmail.com>
2016-01-28 20:31:17 -0500

    try out rendering

--- app.js
+++ app.js
@@ -8,22 +8,36 @@ app.use(morgan('dev'));
 app.listen(process.env.PORT || 3000);
 
 app.get('/special/', function(req, res, next) {
-    console.log('You\'re special');
-    //next();
-});
-app.use('/', function(req, res, next) {
-    res.send(Object.keys(req));
-    // console.log(req.domain)
-    console.log(res.statusCode)
-    console.log(req.method)
-//    console.log(req.statusCode)
-    next();
+	console.log('You\'re special');
+	//next();
 });
 
+app.get('/tacos/', function(req, res, next) {
+	res.send(swig.render({
+
+	}, {
+		locals: {
+			title: 'Tacos!!!',
+			people: {
+				name: 'Taco Stand'
+			}
+
+		},
+		filename: '/views/index.html'
+	}))
+});
+
+app.use('/', function(req, res, next) {
+	res.send(Object.keys(req));
+	// console.log(req.domain)
+	console.log(res.statusCode)
+	console.log(req.method)
+		//    console.log(req.statusCode)
+	next();
+});
 
 
 
 // app.get('/', function(req, res) {
 //     res.send('Welcome');
 // });
-
