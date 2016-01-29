03a52f55241b84b6a5e87b49349c89cf1dd4e7fc
Rob Debbane <studio@robertdebbane.com>
2016-01-28 21:47:46 -0500

    added tweetBank.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views/');
 app.set('cache', false);
-//swig.setDefaults({cache: false})
+swig.setDefaults({cache: false})
 app.use(morgan('dev'));
 
 app.listen(process.env.PORT || 3000);
