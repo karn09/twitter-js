b9db4bb18d9b41c0d6c607842053d7fc2c44cc82
John Nieves <karn09@gmail.com>
2016-01-29 01:07:08 +0000

    basic http request logger

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.listen(process.env.PORT);
+
+app.get('/special/', function(req, res, next) {
+    console.log('You\'re special');
+    //next();
+});
+app.use('/', function(req, res, next) {
+    res.send(Object.keys(req));
+    // console.log(req.domain)
+    console.log(res.statusCode)
+    console.log(req.method)
+//    console.log(req.statusCode)
+    next();
+});
+
+
+
+
+// app.get('/', function(req, res) {
+//     res.send('Welcome');
+// });
+
