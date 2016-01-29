90761598fda8db983067e63ebb46eb4d2c01bd67
John Nieves <karn09@gmail.com>
2016-01-28 21:22:17 -0500

    changes to rendering handling

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var morgan = require('morgan');
-//var swig = require('swig');
+var swig = require('swig');
 var app = express();
-app.engine('html', require('swig').renderFile);
+app.engine('html', swig.renderFile);
+
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views/');
-app.set('cache', 'false');
-
+app.set('cache', false);
+//swig.setDefaults({cache: false})
 app.use(morgan('dev'));
 
 app.listen(process.env.PORT || 3000);
@@ -26,15 +27,14 @@ app.get('/tacos/', function(req, res, next) {
 		}]
 	};
 	console.log('You are here')
-	app.render('index', locals, function(err, output) {
-		console.log(err)
-		res.end(output)
+	res.render('index', locals, function(err, output) {
+		res.send(output)
 	})
 
 
 });
 
-app.use('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
 	//res.send(Object.keys(req));
 	// console.log(req.domain)
 	// console.log(res.statusCode)
@@ -47,11 +47,9 @@ app.use('/', function(req, res, next) {
 			name: 'another'
 		}]
 	};
-	app.render('index', locals, function(err, output) {
-			res.end(output)
-		})
+	res.render('index', locals)
 		//    console.log(req.statusCode)
-	next();
+	//next();
 });
 
 
