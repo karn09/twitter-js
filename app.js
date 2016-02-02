9f95be55e536c5f7af422a5effa3290fed145c78
Rob Debbane <studio@robertdebbane.com>
2016-02-02 11:19:20 -0500

    made a bunch of changes, including moving routes to routes folder

--- app.js
+++ app.js
@@ -11,45 +11,10 @@ app.use(morgan('dev'));
 
 app.listen(process.env.PORT || 3000);
 
-app.get('/special/', function(req, res, next) {
-	console.log('You\'re special');
-	//next();
-});
+var routes = require('./routes/');
+app.use('/', routes);
 
-app.get('/tacos/', function(req, res, next) {
-	var locals = {
-		title: 'Tacos!!!',
-		people: [{
-			name: 'Taco Stand'
-		}, {
-			name: 'another'
-		}]
-	};
-	console.log('You are here')
-	res.render('index', locals, function(err, output) {
-		res.send(output)
-	})
-
-
-});
-
-app.get('/', function(req, res, next) {
-	//res.send(Object.keys(req));
-	// console.log(req.domain)
-	// console.log(res.statusCode)
-	// console.log(req.method)
-	var locals = {
-		title: 'Main!!!',
-		people: [{
-			name: 'Taco Stand'
-		}, {
-			name: 'another'
-		}]
-	};
-	res.render('index', locals)
-		//    console.log(req.statusCode)
-	//next();
-});
+app.use(express.static('public'));
 
 
 
